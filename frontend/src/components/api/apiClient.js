1d42be35812dbb38a8b9c8a2dd81685db04a3d44
pushpakdeore09 <pushpakdeore9114@gmail.com>
2024-11-27 21:50:52 +0530

    updated

--- frontend/src/components/api/apiClient.js
+++ frontend/src/components/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080/"
+    baseURL: "https://payease-server-production.up.railway.app/"
 })
 
 export default apiClient;
